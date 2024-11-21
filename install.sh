f0314d76ad1ea1bdf3d774874dec32fca47cafc8
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-11-21 22:58:17 +0200

    test

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ CURRENT_MODEL=$(cat /sys/class/dmi/id/product_name)
 # Check if the model matches the intended one
 if [[ "$CURRENT_MODEL" == "ROG Strix G512LI_G512LI" ]]; then
     echo "Running script on $CURRENT_MODEL..."
+    sudo systemctl disable --now tuned tuned-ppd
+    sudo pacman -Rns tuned tuned-ppd
+    sudo pacman -S tuned tuned-ppd
+    sudo systemctl enable --now tuned tuned-ppd
     sudo cp -r etc/* /etc
     sudo rm -rf /etc/tuned/ppd.conf.bk
     sudo systemctl restart tuned tuned-ppd
